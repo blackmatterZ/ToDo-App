bfac2be06765a7e8b858b282691a2c9c866949f6
Hurricane <103099890+blackmatterZ@users.noreply.github.com>
2026-09-14 11:41:52 +0300

    refactor(backend): remove unused Like import from todos service

--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Like } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Todo } from './entities/todo.entity';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
